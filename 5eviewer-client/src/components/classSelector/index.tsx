af8552cf248f5146366b0bb2e1fbc528250c0877
lortizg <laura.og179@gmail.com>
2024-10-12 14:49:42 +0200

    Sent logic to child component

--- 5eviewer-client/src/components/classSelector/index.tsx
+++ 5eviewer-client/src/components/classSelector/index.tsx
@@ -1,43 +1,45 @@
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
-import { ClassInfo, SelectedClass, Source } from "../../types";
+import { useEffect, useState } from "react";
+import { ClassInfo, Source } from "../../types";
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 
-export const ClassSelector = (selectedClass: SelectedClass) => {
-	// const [classes, setClasses] = useState([] as ClassInfo[]);
-	// // const [selectedClass, setSelectedClass] = useState<SelectedClass>({ class: '', source: '' });
+export const ClassSelector = ({ selectedClass }: any) => {
+	const [classes, setClasses] = useState([] as ClassInfo[]);
+	const [childSelectedClass, childSetSelectedClass] = useState('|');
 
-	// const getInitialData = async () => {
-	// 	const classListResult = Object.values((await axios.get('https://5e.tools/data/class/index.json')).data) as string[];
+	const getInitialData = async () => {
+		const classListResult = Object.values((await axios.get('https://5e.tools/data/class/index.json')).data) as string[];
 
-	// 	const classesResult = await getClassData(classListResult);
-	// 	setClasses(classesResult);
-	// }
-	// const getClassData = async (classList: any) => {
-	// 	const classesInfo: ClassInfo[] = [];
-	// 	for (const classSource of classList) {
-	// 		const currentClass: ClassInfo = (await axios.get('https://5e.tools/data/class/' + classSource)).data;
-	// 		classesInfo.push(currentClass);
-	// 	};
-	// 	return classesInfo;
-	// }
-	// const handleClassSelection = (e: SelectChangeEvent) => {
-	// 	const selectedArray: string[] = e.target.value.split('|');
-	// 	this.props.selectedClass = ({ class: selectedArray[0], source: selectedArray[1] as Source });
-	// }
-	// useEffect(() => {
-	// 	getInitialData();
-	// }, [])
+		const classesResult = await getClassData(classListResult);
+		setClasses(classesResult);
+	}
+	const getClassData = async (classList: any) => {
+		const classesInfo: ClassInfo[] = [];
+		for (const classSource of classList) {
+			const currentClass: ClassInfo = (await axios.get('https://5e.tools/data/class/' + classSource)).data;
+			classesInfo.push(currentClass);
+		};
+		return classesInfo;
+	}
+	const handleClassSelection = (e: SelectChangeEvent) => {
+		const selectedArray: string[] = e.target.value.split('|');
+		childSetSelectedClass(e.target.value);
+
+		selectedClass({ class: classes[Number(selectedArray[0])], source: selectedArray[1] as Source });
+	}
+	useEffect(() => {
+		getInitialData();
+	}, [])
 
 	// useCallback()
 	return <>
-		{/* {classes.length &&
+		{classes.length &&
 			<FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
 				<InputLabel id="demo-simple-select-standard-label">Class</InputLabel>
 				<Select
 					labelId="demo-simple-select-standard-label"
 					id="demo-simple-select-standard"
-					value={selectedClass.class + '|' + selectedClass.source}
+					value={childSelectedClass}
 					onChange={handleClassSelection}
 					label="Class"
 				>
@@ -47,14 +49,14 @@ export const ClassSelector = (selectedClass: SelectedClass) => {
 					{
 						classes.map((currentClass, i) => {
 							return currentClass.class.map((classSource, sourceindex) => {
-								return <MenuItem value={classSource.name + '|' + classSource.source} key={'class-' + i + '-' + sourceindex}>{classSource.name} - {classSource.source}</MenuItem>
+								return <MenuItem value={i + '|' + classSource.name + '|' + classSource.source} key={'class-' + i + '-' + sourceindex}>{classSource.name} - {classSource.source}</MenuItem>
 							})
 
 						})
 					}
 				</Select >
 			</FormControl >
-		} */}
+		}
 
 	</>
 }
---
+++ 5eviewer-client/src/components/classSelector/types.ts
@@ -0,0 +1,5 @@
+import { SelectedClass } from "../../types"
+
+export type ClassSelectorProps = {
+	selectedClass: SelectedClass
+}
